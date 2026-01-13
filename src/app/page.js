f4e9bfcb77978d618a98581e067964ef33fc24c5
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2026-01-13 20:21:31 +0600

    added partner logo section marquee

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
-import Hero from "./components/Hero";
+import Hero from "@/app/components/Hero";
+import PartnerLogos from "@/app/components/PartnerLogos";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <PartnerLogos />
     </div>
   );
 }
